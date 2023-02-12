1a809322a7587d02e1b6e43ebfb231c519ac510d
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-02-11 23:02:13 -0500

    chakra ui and tailwind added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import reactLogo from "./assets/react.svg";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
