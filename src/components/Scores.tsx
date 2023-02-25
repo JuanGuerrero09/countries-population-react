854ef31bef4a2fc80ceefb663d2f4fbfa211ea68
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-02-25 17:36:37 -0500

    conditional dark mode colors added

--- src/components/Scores.tsx
+++ src/components/Scores.tsx
@@ -1,12 +1,15 @@
+import { useColorMode } from '@chakra-ui/react'
 import React, { useContext } from 'react'
 import { GameContext } from '../context/GameContext'
 
 export default function Scores() {
   const { currentScore, maxScore } = useContext(GameContext)
+  const { colorMode } = useColorMode()
+  const colorWeight = colorMode == 'light'? '600' : '500'
   return (
     <div className="scores flex md:flex-row font-semibold w-screen justify-evenly text-2xl">
-      <h2 className='text-red-600'>Actual score: {currentScore}</h2>
-      <h2 className='text-blue-600'>Max score: {maxScore}</h2>
+      <h2 className={`text-red-${colorWeight}`}>Actual Score: {currentScore}</h2>
+      <h2 className={`text-blue-${colorWeight}`}>Max Score: {maxScore}</h2>
     </div>
   )
 }
