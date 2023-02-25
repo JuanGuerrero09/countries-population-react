5c4e31a25caf438e814ff030852a27f2db7a443b
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-02-25 17:24:13 -0500

    footer added

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import Main from "./components/Main";
 import { ToggleMode } from "./components/ToggleModeBtn";
 import { GameProvider } from "./context/GameContext";
 import Scores from "./components/Scores";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App flex flex-col items-center p-4">
+    <div className="App flex flex-col justify-evenly items-center p-2 md:h-screen">
       <GameProvider>
         <Header />
         <Main />
         <Scores />
         <ToggleMode />
+        <Footer />
       </GameProvider>
     </div>
   );
