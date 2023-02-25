357a4a34262c6cec2c9f4ed06e814f68edc07e98
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-02-24 22:18:25 -0500

    scores placement changed

--- src/components/Scores.tsx
+++ src/components/Scores.tsx
@@ -4,12 +4,9 @@ import { GameContext } from '../context/GameContext'
 export default function Scores() {
   const { currentScore, maxScore } = useContext(GameContext)
   return (
-    <>
-    <div className="scores flex flex-col md:flex-row font-semibold w-screen md:justify-evenly text-xl">
-
+    <div className="scores flex md:flex-row font-semibold w-screen justify-evenly text-2xl">
       <h2 className='text-red-600'>Actual score: {currentScore}</h2>
-      <h2 className='text-blue-600'>Max score {maxScore}</h2>
+      <h2 className='text-blue-600'>Max score: {maxScore}</h2>
     </div>
-    </>
   )
 }
