ec30a914d549a242e8d5c46b4fb8f422abfea43e
JuanGuerrero09 <juanguerrero09mc@gmail.com>
2023-02-28 23:22:21 -0500

    decrecated

--- src/hooks/useGame.tsx
+++ src/hooks/useGame.tsx
@@ -1,10 +1,9 @@
-import { useRef, useState } from "react";
+import { useReducer, useRef, useState, useContext } from "react";
 import { getRandomElement } from "../services/getRandomElement";
 import getRandomCountry from "../services/getRandomCountry";
-
 type CountryInfo = any;
 
-//Useefect para cuando cambie la puntuación
+// Deprecated
 
 // export default async function useCountries(){
 //   const allCountries = await getCountries();
@@ -17,12 +16,13 @@ const initialCountryTwo = getRandomCountry();
 
 
 export function useGame() {
+  // const lastCountry = useRef(secondCountry);
+
   const [firstCountry, setFirstCountry] =
     useState<CountryInfo>(initialCountryOne);
   const [secondCountry, setSecondCountry] =
     useState<CountryInfo>(initialCountryTwo);
   const [gameOver, setGameOver] = useState<boolean>(false);
-  const lastCountry = useRef(secondCountry);
   const [currentScore, setCurrentScore] = useState(0);
   const [maxScore, setMaxScore] = useState(0);
 
@@ -31,6 +31,7 @@ export function useGame() {
     getRandomElement(possibleQuestions)
   );
 
+  
   const handleQuestion = (event: React.MouseEvent<HTMLButtonElement>) => {
     const answer = (event.target as HTMLButtonElement).value;
     const isAnswerCorrect =
@@ -38,9 +39,9 @@ export function useGame() {
         answer === "less") ||
       (firstCountry[question] <= secondCountry[question] && answer === "more");
     if (isAnswerCorrect) {
-      const newCountry = getRandomCountry();
       const newQuestion = getRandomElement(possibleQuestions);
       setQuestion(newQuestion);
+      const newCountry = getRandomCountry();
       setCurrentScore(currentScore + 1);
       setFirstCountry(secondCountry);
       setSecondCountry(newCountry);
